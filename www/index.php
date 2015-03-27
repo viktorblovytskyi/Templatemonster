cd9605485e7e59ec54c7fdf18316d5167f1d366d
Viktor Blovytskyi <leito3101@gmail.com>
2015-03-28 01:18:35 +0200

    ver. 0.1: authentication

---
+++ www/index.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+    mysql_connect('localhost','root') or die(mysql_error());
+    mysql_select_db('banadmin') or die(mysql_error());
+
+    include('authentication.php');
+
+    if(USER_LOGGED) {
+    if(!check_user($user_id)) logout();
+    ?>
+    <h1>Здравствуйте, <?php echo $login; ?>!</h1>
+    <h2>Ваш ID: <?php echo $user_id; ?>.</h2>
+    <h4><a href="?logout">Выход</a></h4>
+<?php
+}
+else { ?>
+    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <table>
+            <tr>
+                <td>Имя:</td><td><input type="text" name="user"></td>
+            </tr>
+            <tr>
+                <td>Пароль:</td><td><input type="password" name="pass"></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit" name="login" value="Войти"></td>
+            </tr>
+        </table>
+    </form>
+<?php
+}
+?>
